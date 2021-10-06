1ee8eca3c16942cd554b3adf8505c897206ed83d
Viktoria <zaharova181114@gmail.com>
2021-10-07 01:41:25 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -11,11 +11,51 @@ $('.choosing-a-home-slider').slick({
     variableWidth: true,
     appendArrows: '.choosing-a-home-slider__nav',
     prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+
+        {
+            breakpoint: 768,
+            settings: {
+                variableWidth: false,
+                slidesToShow: 1,
+            }
+        }
+
+    ]
 });
 
 $('.news-slider').slick({
     slidesToShow: 4,
     prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+
+    ]
+});
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
 });
