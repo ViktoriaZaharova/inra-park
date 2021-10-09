3a256701a52c862d5f615b6dcb76b0cdab3a2184
Viktoria <zaharova181114@gmail.com>
2021-10-09 14:51:57 +0300

    img compress

--- app/js/common.js
+++ app/js/common.js
@@ -59,3 +59,19 @@ $('.btn-burger').on('click', function () {
 $('.btn-close').on('click', function () {
     $('.mobile-menu').fadeOut();
 });
+
+//плавный скролл
+$('.go_to').on('click', function (e) {
+    e.preventDefault();
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length != 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    $('.mobile-menu').fadeOut();
+    return false;
+});
+//плавный скролл end
+
+$('[name="phone"]').mask('+7 (999) 999-99-99');
