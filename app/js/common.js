0752f5eccfeaba0db30889ab0d72f5c794d56e4e
Viktoria <zaharova181114@gmail.com>
2021-11-17 19:46:03 +0200

    edit2

--- app/js/common.js
+++ app/js/common.js
@@ -8,51 +8,7 @@ $('.home-slider').slick({
     // nextArrow: '<button type="button" class="slick-next"></button>'
 });
 
-$('.choosing-a-home-slider').slick({
-    slidesToShow: 3,
-    variableWidth: true,
-    appendArrows: '.choosing-a-home-slider__nav',
-    prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>',
-    responsive: [
 
-        {
-            breakpoint: 768,
-            settings: {
-                variableWidth: false,
-                slidesToShow: 1,
-            }
-        }
-
-    ]
-});
-
-$('.news-slider').slick({
-    slidesToShow: 4,
-    prevArrow: '<button type="button" class="slick-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"></button>',
-    responsive: [
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 3,
-            }
-        },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2,
-            }
-        },
-        {
-            breakpoint: 576,
-            settings: {
-                slidesToShow: 1,
-            }
-        }
-
-    ]
-});
 
 $('.btn-burger').on('click', function () {
    $('.mobile-menu').fadeToggle();
